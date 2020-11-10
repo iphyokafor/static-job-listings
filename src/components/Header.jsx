2b5921d0093abb051e790aa9b53ab9ca57468a38
iphyokafor <sandraifeoma22@gmail.com>
2020-11-10 09:28:59 +0100

    Set up project achietecture

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div
+      style={{
+        background: "cyan",
+        width: "100%",
+        height: "80px",
+        margin: "-3",
+      }}>
+      <div>DevJobs</div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/utils/Card.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class card extends Component {
+  render() {
+    return <div className="card">Card component</div>;
+  }
+}
+
+export default card;
